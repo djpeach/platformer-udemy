4e518c4f883fa1b2afe8b63245075fdb88fbf2a7
Daniel Peach <dpeaches96@gmail.com>
2020-05-03 22:06:03 -0400

    rearranged code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
-/// <reference path="../types/phaser.d.ts" />
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import config from './config';
+import GameScene from './scenes/Game';
 
-const config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-  },
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image('logo', logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, 'logo');
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: 'Power2',
-    yoyo: true,
-    loop: -1,
-  });
-}
+new Phaser.Game(
+  Object.assign(config, {
+    scene: [GameScene],
+  })
+);
---
+++ src/scenes/Game.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  init(data) {}
+
+  preload() {
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+  }
+
+  create(data) {
+    this.add.sprite(400, 300, 'hero-run-sheet', 5);
+  }
+
+  update(time, delta) {}
+}
+
+export default Game;
